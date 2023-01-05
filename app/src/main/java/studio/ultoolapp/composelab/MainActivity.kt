89a7a746c0b2ad15203baf9e7efe4a1173b98c97
Samuel Chou <samuelchou@hotmail.com.tw>
2023-01-05 16:51:57 +0800

    feat: 示範如何透過 modifier 修飾子進行排版。

--- app/src/main/java/studio/ultoolapp/composelab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/composelab/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import studio.ultoolapp.composelab.ui.theme.ComposeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +36,18 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     val image = painterResource(id = R.drawable.androidparty)
     Box {
-        Image(painter = image, contentDescription = "")
-        Text(text = "Hello $name!")
+        Image(
+            painter = image, contentDescription = "",
+            modifier = Modifier.fillMaxHeight(),
+            contentScale = ContentScale.Crop,
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+                .padding(16.dp)
+        )
     }
 }
 
